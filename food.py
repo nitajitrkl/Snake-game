444a2c6985920b16595b80280f3e978e8f0d72f6
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2022-01-07 09:52:34 +0530

    V3.15

--- food.py
+++ food.py
@@ -9,7 +9,7 @@ class Food(Turtle):
         self.shape("triangle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("white")
+        self.color("red")
         self.speed("fastest")
         self.refresh()
 
